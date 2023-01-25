259bc5e26dc1cda9637d5f916f396f1d4a4668ba
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-01-26 00:38:24 +0100

    sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,88 +1,78 @@
-import { BsGrid, BsArrowUp } from 'react-icons/bs';
-import { AiOutlinePlus } from 'react-icons/ai';
-import { RiUser3Line } from 'react-icons/ri';
-import { MdOutlineLogout } from 'react-icons/md';
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom'
+//import useState hook to create menu collapse state
+import React, { useState } from "react";
 
-const SideBar = () => {
-  const [routes, setRoutes] = useState('/dashboard');
-  const dispatch = useDispatch();
+//import react pro sidebar components
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
+
+//import icons from react icons
+import { FaList, FaRegHeart } from "react-icons/fa";
+import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import { RiPencilLine } from "react-icons/ri";
+import { BiCog } from "react-icons/bi";
 
 
-  const dataSide = [
-    {icon: BsGrid, desc: 'Dashboard', to: '/dashboard'},
-    {icon: BsArrowUp, desc: 'Transfer', to: '/transfer'},
-    {icon: AiOutlinePlus, desc: 'Top Up', to: '/top-up'},
-    {icon: RiUser3Line, desc: 'Profile', to: '/profile'}
-  ]
+//import sidebar css from react-pro-sidebar module and our custom css 
+import "react-pro-sidebar/dist/css/styles.css";
+import "./Sidebar.css";
 
-  const handleTopup = (e) => {
-    e.preventDefault();
-    
-  }
 
-  const handleLogout = (e) => {
-    e.preventDefault();
-    dispatch({
-      type: 'LOGOUT'
-    })
+const Header = () => {
+  
+    //create initial menuCollapse state using useState hook
+    const [menuCollapse, setMenuCollapse] = useState(false)
 
-  }
+    //create a custom function that will change menucollapse state from false to true and true to false
+  const menuIconClick = () => {
+    //condition checking to change state from true to false and vice versa
+    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
+  };
 
   return (
-    <div className='card p-4 bg-white position-relative h-100'>
-        {dataSide.map((data, index) => {
-          return <Link href={data.to} key={data.desc} className=' d-flex flex-row align-items-center my-3'>
-
-          </Link>
-        })}
-        <button event={handleLogout} className=' d-flex flex-row align-items-center my-3'>
-          <a className='text-decoration-none text-white my-4 ps-3 position-absolute bottom-0'>
-            <MdOutlineLogout className='fs-5 me-2 text-dark'/>
-            <span className='text-dark'>Logout</span>
-          </a>
-        </button>  
-        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content bg-primary text-dark">
-              <div className="modal-header border-bottom-0">
-                <h5 className="modal-title" id="exampleModalLabel">Topup</h5>
-                <button onClick={e => ('/top-up')} type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <p className="ps-3">Enter the amount of money, and click submit</p>
-              <div className="modal-body">
-                <form>
-                  <input className={`form-control text-center `} type='number' placeholder="___________" />
-                </form>
-              </div>
-              <div className="modal-footer border-top-0">
-                <button onClick={e => ('/top-up')} type="button" className="btn btn-light" data-bs-dismiss="modal">Submit</button>
-              </div>
+    <>
+      <div id="header">
+          {/* collapsed props to change menu size using menucollapse state */}
+        <ProSidebar collapsed={menuCollapse}>
+          <SidebarHeader>
+          <div className="logotext">
+              {/* small and big change using menucollapse state */}
+              <p>{menuCollapse ? "Logo" : "Big Logo"}</p>
             </div>
-          </div>
-        </div>
+            <div className="closemenu" onClick={menuIconClick}>
+                {/* changing menu collapse icon on click */}
+              {menuCollapse ? (
+                <FiArrowRightCircle/>
+              ) : (
+                <FiArrowLeftCircle/>
+              )}
+            </div>
+          </SidebarHeader>
+          <SidebarContent>
+            <Menu iconShape="square">
+              <MenuItem active={true} icon={<FiHome />}>
+                Home
+              </MenuItem>
+              <MenuItem icon={<FaList />}>Category</MenuItem>
+              <MenuItem icon={<FaRegHeart />}>Favourite</MenuItem>
+              <MenuItem icon={<RiPencilLine />}>Author</MenuItem>
+              <MenuItem icon={<BiCog />}>Settings</MenuItem>
+            </Menu>
+          </SidebarContent>
+          <SidebarFooter>
+            <Menu iconShape="square">
+              <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
+            </Menu>
+          </SidebarFooter>
+        </ProSidebar>
       </div>
-    //   <aside className='col-lg-4'>
-    //   <div className='card p-4 bg-light position-relative'>
-    //     {dataSide.map((data, index) => {
-    //       return <Link href={data.to} key={data.desc} className=' d-flex flex-row align-items-center my-3'>
-    //         <a className={`${routes === data.to && 'active-side'} side-side text-decoration-none text-white my-4 ps-2`}>
-    //           <data.icon className='fs-5 me-2'/>
-    //           <span>{data.desc}</span>
-    //         </a>
-    //       </Link>
-    //     })}
-    //     <Link href='/login' className=' d-flex flex-row align-items-center my-3 position-absolute'>
-    //       <a className='text-decoration-none text-white my-4 ps-3'>
-    //         <MdOutlineLogout className='fs-5 me-2'/>
-    //         <span>Logout</span>
-    //       </a>
-    //     </Link>
-    //   </div>
-    // </aside>
-  )
-}
+    </>
+  );
+};
 
-export default SideBar
+export default Header;
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,70 +1,11 @@
-import { Link } from 'react-router-dom';
-import './Dashboard.scss'
-import { Row, Col } from 'react-bootstrap';
-import SideBar from '../../components/sidebar/Sidebar';
-import { BsArrowUp } from 'react-icons/bs';
-import ChartCard from '../../components/chartCard/ChartCard';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-// import { getBalance } from '../redux/actions/profile';
-import nominalFormat from '../../helpers/nominalFormat';
+import React from 'react'
+import { Sidebar  } from "../../components/sidebar/Sidebar"
 
 const Dashboard = () => {
-
-
-
   return (
-    <>
-    <div className=''>
-
+    <div>
+      <Sidebar />
     </div>
-      <main className=''>
-        <Row className=''>
-          <aside className='col-lg-4'>
-            <SideBar />
-          </aside>
-          <section className='col-lg-8'>
-            <div className='card p-4 bg-primary'>
-              <Row className='text-dark'>
-                <Col xs={12} md={6} className='d-flex justify-content-between flex-column'>
-                  <div className='text-light'>Balance</div>
-                  <h2 className='text-light'>Rp {nominalFormat}</h2>
-                  
-                </Col>
-                <Col xs={12} md={6} className='d-flex justify-content-end'>
-                  <div className='d-flex flex-column'>
-                    <Link href='/transfer' className=' d-flex flex-row align-items-center my-2'>
-                      <a className='text-decoration-none text-white my-2 py-3 px-5 bg-secondary btn'>
-                        <BsArrowUp className='fs-5 me-2 text-dark'/>
-                        <span className='text-dark'>Transfer</span>
-                      </a>
-                    </Link>
-                    <Link href='/top-up' className=' d-flex flex-row align-items-center my-2'>
-                      <a className='text-decoration-none text-white my-2 py-3 px-5 bg-secondary btn'>
-                        <BsArrowUp className='fs-5 me-2 text-dark'/>
-                        <span className='text-dark'>Top Up</span>
-                      </a>
-                    </Link>
-                  </div>
-                </Col>
-              </Row>
-            </div>
-            <Row>
-              <Col xs={12} lg={6} className='mt-3'>
-                <div className='card bg-white py-3 h-100'>
-                  <ChartCard dataChart={[120, 50, 70, 80, 65, 100, 90]} labels={['Sat','Sun','Mon','Tue', 'Wed', 'Thu', 'Fri']} income={1800000} expense={1200000} />
-                </div>
-              </Col>
-              <Col xs={12} lg={6} className='mt-3'>
-                <div className='card bg-white p-3 h-100'>
-                  <h4>Transaction History</h4>
-                </div>
-              </Col>
-            </Row>
-          </section>
-        </Row>
-      </main>
-    </>
   )
 }
 
